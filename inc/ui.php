1d92bedfd2606cdf9aa688ecb9e924f4f83a0a82
jcskyrock <jonathan@webshop.bb>
2013-11-26 18:26:23 -0400

    PHP error fixes

--- inc/ui.php
+++ inc/ui.php
@@ -147,17 +147,30 @@ function taxonomy_filter_restrict_manage_posts() {
     	$filters = get_object_taxonomies( $typenow );
 
         foreach ( $filters as $tax_slug ) {
-            $tax_obj = get_taxonomy( $tax_slug );
-            wp_dropdown_categories( array(
-                'show_option_all' => __('Show All '.$tax_obj->label ),
-                'taxonomy' 	  => $tax_slug,
-                'name' 		  => $tax_obj->name,
-                'orderby' 	  => 'name',
-                'selected' 	  => $_GET[$tax_slug],
-                'hierarchical' 	  => $tax_obj->hierarchical,
-                'show_count' 	  => false,
-                'hide_empty' 	  => true
-            ) );
+			if ( isset( $_GET[$tax_slug] ) ) {
+				$tax_obj = get_taxonomy( $tax_slug );
+				wp_dropdown_categories( array(
+					'show_option_all' => __('Show All '.$tax_obj->label ),
+					'taxonomy' 	  => $tax_slug,
+					'name' 		  => $tax_obj->name,
+					'orderby' 	  => 'name',
+					'selected' 	  => $_GET[$tax_slug],
+					'hierarchical' 	  => $tax_obj->hierarchical,
+					'show_count' 	  => false,
+					'hide_empty' 	  => true
+				) );
+			} else {
+				$tax_obj = get_taxonomy( $tax_slug );
+				wp_dropdown_categories( array(
+					'show_option_all' => __('Show All '.$tax_obj->label ),
+					'taxonomy' 	  => $tax_slug,
+					'name' 		  => $tax_obj->name,
+					'orderby' 	  => 'name',
+					'hierarchical' 	  => $tax_obj->hierarchical,
+					'show_count' 	  => false,
+					'hide_empty' 	  => true
+				) );
+			}
         }
     }
 }
@@ -168,18 +181,22 @@ add_action( 'restrict_manage_posts', 'taxonomy_filter_restrict_manage_posts' );
  */
 
 function taxonomy_filter_post_type_request( $query ) {
-  global $pagenow, $typenow;
 
-  if ( 'edit.php' == $pagenow ) {
-    $filters = get_object_taxonomies( $typenow );
-    foreach ( $filters as $tax_slug ) {
-      $var = &$query->query_vars[$tax_slug];
-      if ( isset( $var ) ) {
-        $term = get_term_by( 'id', $var, $tax_slug );
-        $var = $term->slug;
-      }
-    }
-  }
+	global $pagenow, $typenow;
+
+	if ( 'edit.php' == $pagenow ) {
+		$filters = get_object_taxonomies( $typenow );
+		foreach ( $filters as $tax_slug ) {
+			$var = &$query->query_vars[$tax_slug];
+			if ( isset( $var ) ) {
+				$term = get_term_by( 'id', $var, $tax_slug, 'OBJECT' );
+				if ( is_object( $term ) ) {
+					$var = $term->slug;
+				}
+			}
+		}
+	}
+
 }
 add_filter( 'parse_query', 'taxonomy_filter_post_type_request' );
 
@@ -189,7 +206,7 @@ add_filter( 'parse_query', 'taxonomy_filter_post_type_request' );
 
 function rel_search_join( $join ) {
 	global $pagenow, $wpdb;
-	if ( is_admin() && $pagenow == 'edit.php' && $_GET['post_type'] == 'listings' && $_GET['s'] != '') {    
+	if ( is_admin() && $pagenow == 'edit.php' && $_GET['post_type'] == 'listings' && isset( $_GET['s'] ) != '') {    
 		$join .= 'LEFT JOIN ' . $wpdb->postmeta . ' ON '. $wpdb->posts . '.ID = ' . $wpdb->postmeta . '.post_id ';
 	}
 	return $join;
@@ -198,7 +215,7 @@ add_filter('posts_join', 'rel_search_join' );
 
 function rel_search_where( $where ) {
 	global $pagenow, $wpdb;
-	if ( is_admin() && $pagenow == 'edit.php' && $_GET['post_type']=='listings' && $_GET['s'] != '' ) {
+	if ( is_admin() && $pagenow == 'edit.php' && $_GET['post_type']=='listings' && isset( $_GET['s'] ) != '' ) {
 		$where = preg_replace( "/\(\s*".$wpdb->posts.".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/", "(".$wpdb->posts.".post_title LIKE $1) OR (".$wpdb->postmeta.".meta_value LIKE $1)", $where );
 	}
 	return $where;
@@ -207,7 +224,7 @@ add_filter( 'posts_where', 'rel_search_where' );
 
 function rel_post_limits( $groupby ) {
     global $pagenow, $wpdb;
-    if ( is_admin() && $pagenow == 'edit.php' && $_GET['post_type']=='listings' && $_GET['s'] != '' ) {
+    if ( is_admin() && $pagenow == 'edit.php' && $_GET['post_type']=='listings' && isset( $_GET['s'] ) != '' ) {
         $groupby = "$wpdb->posts.ID";
     }
     return $groupby;
